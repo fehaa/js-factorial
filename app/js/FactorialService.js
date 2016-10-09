16f59381f3337ec770566d738cff117da9879dad
Fernando Alves <fehaa2000@gmail.com>
2016-10-09 11:15:45 -0300

    Setup test enviroment

---
+++ app/js/FactorialService.js
@@ -0,0 +1,11 @@
+var FactorialService = (function() {
+
+    function calculate(number) {
+        return 120;
+    }
+
+    //public api
+    return {
+        "calculate" : calculate
+    };
+})();
---
+++ test/FactorialServiceSpec.js
@@ -0,0 +1,6 @@
+describe('FactorialService.js', function(){
+
+    it('should calculate the factorial from a given number', function() {
+        expect(FactorialService.calculate(5)).toBe(120);
+    });
+});
